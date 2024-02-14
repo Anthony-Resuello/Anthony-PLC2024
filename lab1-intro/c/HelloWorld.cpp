6ea9738bdadb5469175dc7c635b52cf7905b0c64
Anthony Resuello <anthonyresuello2004@gmail.com>
2024-02-14 18:21:01 +0000

    Run C and C++ Program

---
+++ lab1-intro/c/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+cout << "Type a number: "; // Type a number and press enter cin >> n; // Get user input from the keyboard
+while ( n = 0 ) {
+cout << "Non-Zero Number please: ";
+cin >> n; }
+cout << "n = " << n <<" Good-bye";
+return 0; }
